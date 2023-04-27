894e305de50033e5a1368cf46a26523e10b1cd13
Feratin0s <arthurdealmeidaferraz1@gmail.com>
2023-04-27 02:20:21 -0300

    Versao 1.0 LAYOUT concluido

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ const userName = document.querySelector('.user-name');
 userName.textContent = user;
 
 //Para mudar a borda quando clicar na roupa pra selecionar imagem
-function mudarBorda() {
-    var divs = document.querySelectorAll('.circulo');
-    divs.forEach(function(div) {
-      div.addEventListener('click', function() {
-        this.style.border = '2px solid purple';
-      });
-    });
-  }
-  
+function mudarBorda(elemento) {
+const grupoRoupas = elemento.parentElement.parentElement;
+const opcoesRoupas = grupoRoupas.querySelectorAll('.circulo');
+for (let i = 0; i < opcoesRoupas.length; i++) {
+if (opcoesRoupas[i] === elemento) {
+opcoesRoupas[i].style.border = '2px solid purple';
+} else {
+opcoesRoupas[i].style.border = 'none';
+}
+}
+
+const botao = document.querySelector('.comprar');
+const opcoesSelecionadas = document.querySelectorAll('.selecionarroupas .circulo[style*="purple"]');
+if (opcoesSelecionadas.length === 3) {
+botao.style.backgroundColor = 'purple';
+botao.onclick = function() { alert('TESTANDO TESTANDO APAGAR DEPOIS'); }
+} else {
+botao.style.backgroundColor = 'gray';
+botao.onclick = null;
+}
+}
