eaf3bc7ff60cdefb1e7d89ffebf96815f2b838e9
Feratin0s <arthurdealmeidaferraz1@gmail.com>
2023-04-27 06:05:04 -0300

    Versao 1.3 com POP-UP

--- script.js
+++ script.js
@@ -20,8 +20,21 @@ const botao = document.querySelector('.comprar');
 const opcoesSelecionadas = document.querySelectorAll('.selecionarroupas .circulo[style*="purple"]');
 if (opcoesSelecionadas.length === 3) {
 botao.style.backgroundColor = '#404EED';
-botao.onclick = function() { alert('TESTANDO TESTANDO APAGAR DEPOIS'); }
+botao.onclick = function() { document.getElementById("popup").style.display = "block"; }
 } else {
 botao.onclick = null;
 }
+}
+
+// Esconder os POP-UPS
+
+function hide() {
+    document.getElementById("popup").style.display = "none";
+    document.getElementById("direita").style.display = "flex";
+    document.getElementById("direita2").style.display = "none";
+  }
+
+function trocarpopup() {
+document.getElementById("direita").style.display = "none";
+document.getElementById("direita2").style.display = "flex";
 }
