1860780f9f5c2623c2c156a753e92fbc95cc9e76
Feratin0s <arthurdealmeidaferraz1@gmail.com>
2023-04-26 22:42:33 -0300

    Layout-Chooseyourmodel-select

--- script.js
+++ script.js
@@ -3,3 +3,14 @@
 const user = prompt("Qual o seu nome?")
 const userName = document.querySelector('.user-name');
 userName.textContent = user;
+
+//Para mudar a borda quando clicar na roupa pra selecionar imagem
+function mudarBorda() {
+    var divs = document.querySelectorAll('.circulo');
+    divs.forEach(function(div) {
+      div.addEventListener('click', function() {
+        this.style.border = '2px solid purple';
+      });
+    });
+  }
+  
